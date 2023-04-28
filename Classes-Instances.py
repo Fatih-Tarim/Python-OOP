d1ec0aaef1dd708df50d8647bef22233b132ff9b
Fatih-Tarim <fatihtrm99@gmail.com>
2023-04-28 13:53:48 +0300

    Part IV

--- Classes-Instances.py
+++ Classes-Instances.py
@@ -1,31 +1,32 @@
 class Personel:
-    #class variables
+    # class variables
     zam_orani = 1.05
     personel_sayisi = 0
 
-    #Consturctor
+    # Consturctor
     def __init__(self, isim, soyisim, maas):
         self.isim = isim.title()
         self.soyisim = soyisim
         self.maas = maas
         self.email = f"{isim.lower()}.{soyisim.lower()}@firmam.com"
-        Personel.personel_sayisi +=1
+        Personel.personel_sayisi += 1
+        #print(f"Yeni Personel Tanımlandı: {isim} {soyisim}")
 
     def tam_isim(self):
         return f"{self.isim} {self.soyisim}"
-    
+
     def zam_uygula(self):
         self.maas = int(self.maas * self.zam_orani)
-    
+
     @classmethod
     def zam_oranini_belirle(cls, oran):
-        eski_oran= cls.zam_orani
+        eski_oran = cls.zam_orani
         cls.zam_orani = oran
-        print(f"Zam oranı değiştirildi eski zam oranı ({eski_oran}). Yeni zam oranı ({cls.zam_orani}) ")
-    
+        #print(f"Zam oranı değiştirildi eski zam oranı ({eski_oran}). Yeni zam oranı ({cls.zam_orani}) ")
+
     @classmethod
     def from_string(cls, per_str):
-        isim,soyisim,maas = per_str.split("-")
+        isim, soyisim, maas = per_str.split("-")
         return cls(isim, soyisim, maas)
 
     @staticmethod
@@ -35,10 +36,67 @@ class Personel:
         else:
             return "Hafta içi"
 
-    
+# Method Resolution Order
 
-per_1 = Personel(isim="John", soyisim="Smith", maas=12000)
-per_2 = Personel("Mary","Smith",15000)
+
+class Yazilimci(Personel):
+    zam_orani = 1.1
+
+    def __init__(self, isim, soyisim, maas, dil):
+        super().__init__(isim, soyisim, maas)
+        self.dil = dil
+        #print(f"Yeni Personel Yazılımcı Kategorisine Taşındı: {self.isim} {self.soyisim}")
+
+
+class Mudur(Personel):
+    def __init__(self, isim, soyisim, maas, personeller= None):
+        super().__init__(isim, soyisim, maas)
+        if personeller is None:
+            self.personeller = []
+        else:
+            self.personeller = personeller
+     
+
+    def personle_ekle(self, per):
+        if per not in self.personeller:
+            self.personeller.append(per)
+    
+    def personle_sil(self, per):
+        if per in self.personeller:
+            self.personeller.remove(per)
+    
+    def personelleri_listele(self):
+        for e, per in enumerate(self.personeller):
+            e += 1
+            print(e, per.tam_isim())
+
+
+yazilimci_1 = Yazilimci(isim="John", soyisim="Smith", maas=12000, dil="python")
+yazilimci_2 = Yazilimci("Mary", "Smith", 15000, "Java")
+
+mdr_1 = Mudur('John', 'Wick', 70000, [yazilimci_1])
+mdr_2s = Mudur('John', 'Snow', 70000)
+
+
+#isinstance()
+print(isinstance(yazilimci_1, Personel))
+
+#issubclass()
+print(issubclass(Yazilimci, Personel))
+
+
+
+# print(mdr_1.tam_isim())
+# print("------------------------")
+# mdr_1.personelleri_listele()
+# print("------------------------")
+# mdr_1.personle_ekle(yazilimci_2)
+# mdr_1.personelleri_listele()
+
+# print(yazilimci_1.dil)
+# yazilimci_1.zam_uygula()
+# #print(yazilimci_2.dil)
+
 
 # print(per_1.isim)
 # print(per_1.soyisim)
@@ -68,17 +126,16 @@ per_2 = Personel("Mary","Smith",15000)
 # print(per_1.zam_orani)
 # print(per_2.zam_orani)
 
-per_str_1 = "Sam-Winchester-40000"
-per_str_2 = "Dean-Winchester-60000"
-per_str_3 = "Bobby-Singer-60000"
+# per_str_1 = "Sam-Winchester-40000"
+# per_str_2 = "Dean-Winchester-60000"
+# per_str_3 = "Bobby-Singer-60000"
 
-yeni_per = Personel.from_string(per_str_1)
+# yeni_per = Personel.from_string(per_str_1)
 
-print(yeni_per.email)
-print(yeni_per.maas)
+# print(yeni_per.email)
+# print(yeni_per.maas)
 
-import datetime
-tarih = datetime.date(2022,4,6)
-print(tarih.day)
-print(Personel.mesai_gunu(tarih))
-
+# import datetime
+# tarih = datetime.date(2022,4,6)
+# print(tarih.day)
+# print(Personel.mesai_gunu(tarih))
